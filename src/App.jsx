76eefb93ea5ead7e29c42d377cb1cf0caa81c3a1
pastel DeQueijo <kauan.soliveira@eaportal.org>
2024-06-07 00:58:25 +0000

    style: criação dos estilos globais

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { ThemeProvider } from "styled-components"
 import { theme } from "./styles/theme.js"
+import { GlobalStyle } from "./styles/global.js"
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,7 @@ export function App() {
   return (
     <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
+      <GlobalStyle></GlobalStyle>
     </ThemeProvider>
   )
 }
